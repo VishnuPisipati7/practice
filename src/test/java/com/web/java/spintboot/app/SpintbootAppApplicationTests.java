046c55c0f9358ec3ff329c1464ee032d9083f176
VishnuPisipati7 <143344504+VishnuPisipati7@users.noreply.github.com>
2023-08-27 16:48:41 -0500

    commit all

---
+++ src/test/java/com/web/java/spintboot/app/SpintbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.java.spintboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpintbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
